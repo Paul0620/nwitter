73096a7568298c503d10dbc8f7a84c99529af73f
paul0620 <dlrhkdghsp@gmail.com>
2022-10-26 22:54:04 +0900

    추가 수정

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// github에 올리지 않도록 .env에 값을 담고 ignore처리 하지만 실제 빌드를 하고 올릴때는 들어감
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+export default initializeApp(firebaseConfig);
