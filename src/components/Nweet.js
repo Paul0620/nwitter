df80ac104fa62152d1d67fbdebc2c151b4f020d0
paul0620 <dlrhkdghsp@gmail.com>
2022-11-03 23:25:14 +0900

    update 삭제 수정 추가

---
+++ src/components/Nweet.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { dbService } from "fBase";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+
+function Nweet({ nweetObj, isOwner }) {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+  // 삭제, 수정 -> doc(getFirestore()을 담은 변수, 콜렉션 명, 데이터 id값)
+  const nweetDoc = doc(dbService, "nweets", nweetObj.id);
+
+  // 삭제
+  const onDeleteClick = async () => {
+    const ok = window.confirm("정말 삭제하시겠습니까?");
+
+    if (ok) {
+      // 삭제
+      await deleteDoc(nweetDoc);
+    }
+  };
+
+  // 수정폼 or 리스트
+  const toggleEditing = () => {
+    setEditing((prev) => !prev);
+  };
+
+  // 수정 제출
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await updateDoc(nweetDoc, { text: newNweet });
+
+    setEditing(false);
+  };
+
+  // 수정
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setNewNweet(value);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="수정"
+              value={newNweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="newNweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,36 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { dbService } from "fBase";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import Nweet from "components/Nweet";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
 
-  // 데이터 가져오기
-  const getNweets = async () => {
-    const dbNweets = await getDocs(collection(dbService, "nweets"));
-    dbNweets.forEach((document) => {
-      const nweetObject = {
-        ...document.data(),
-        id: document.id,
-      };
+  // 데이터 가져오기 ->  오래된 방식?
+  // const getNweets = async () => {
+  //   const dbNweets = await getDocs(collection(dbService, "nweets"));
+  //   dbNweets.forEach((document) => {
+  //     const nweetObject = {
+  //       ...document.data(),
+  //       id: document.id,
+  //     };
 
-      setNweets((prev) => [nweetObject, ...prev]);
-    });
-  };
+  //     setNweets((prev) => [nweetObject, ...prev]);
+  //   });
+  // };
 
   useEffect(() => {
-    getNweets();
+    // getNweets();
+    // snapshot을 통해 데이터 가져오기 -> re-render를 하지 않음
+    // 실시간 업데이트
+    onSnapshot(collection(dbService, "nweets"), (snapshot) => {
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setNweets(nweetArray);
+    });
   }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
+    if (nweet.length === 0) {
+      alert("내용을 넣어주세요");
+      return;
+    }
+
     // 콜렉션 생성(테이블?)
     try {
       await addDoc(collection(dbService, "nweets"), {
-        nweet,
+        text: nweet,
         createdAt: Date.now(),
+        creatorId: userObj.uid,
       });
     } catch (error) {
       console.error(error);
@@ -63,9 +80,11 @@ const Home = () => {
 
       <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.nweet}</h4>
-          </div>
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
         ))}
       </div>
     </div>
